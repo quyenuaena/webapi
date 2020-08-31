f21d9588155bb2a254ebd9774434b69696c04a36
Thanh Quyen <thanhquyen270899@gmail.com>
2020-08-31 13:31:14 +0700

    Initial entities
    Initial entities

---
+++ WebSummer/Data/EFCore/EShopDBContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+namespace Data.EFCore
+{
+    public class EShopDBContext : DbContext
+    {
+        public EShopDBContext([NotNullAttribute] DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
