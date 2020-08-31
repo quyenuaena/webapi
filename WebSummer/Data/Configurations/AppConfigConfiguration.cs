5e564dd5b9c5e9ddfe45d25514490c4fdabf8fa9
Thanh Quyen <thanhquyen270899@gmail.com>
2020-08-31 18:34:21 +0700

    Migration first database
    Migration first database using Fluent API

---
+++ WebSummer/Data/Configurations/AppConfigConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Data.Configurations
+{
+    class AppConfigConfiguration : IEntityTypeConfiguration<AppConfig>
+    {
+        public void Configure(EntityTypeBuilder<AppConfig> builder)
+        {
+            builder.ToTable("AppConfigs");
+            builder.HasKey(x => x.Key);
+            builder.Property(x => x.Value).IsRequired();
+        }
+    }
+}
--- WebSummer/Data/EFCore/EShopDBContext.cs
+++ WebSummer/Data/EFCore/EShopDBContext.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.Text;
 using Data.Entities;
+using Data.Configurations;
+
 namespace Data.EFCore
 {
     public class EShopDBContext : DbContext
@@ -11,6 +13,46 @@ namespace Data.EFCore
         public EShopDBContext([NotNullAttribute] DbContextOptions options) : base(options)
         {
         }
-        public DbSet<Product> Products { get; set; };
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new AppConfigConfiguration());
+            modelBuilder.ApplyConfiguration(new CartConfiguration());
+
+            modelBuilder.ApplyConfiguration(new ProductConfiguration());
+            modelBuilder.ApplyConfiguration(new CategoryConfiguration());
+            modelBuilder.ApplyConfiguration(new ProductInCategoryConfiguration());
+            modelBuilder.ApplyConfiguration(new OrderConfiguration());
+
+            modelBuilder.ApplyConfiguration(new OrderDetailConfiguration());
+            modelBuilder.ApplyConfiguration(new ContactConfiguration());
+            modelBuilder.ApplyConfiguration(new PromotionConfiguration());
+            modelBuilder.ApplyConfiguration(new TransactionConfiguration());
+            //base.OnModelCreating(modelBuilder);
+        }
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<Category> Categories { get; set; }
+
+        public DbSet<AppConfig> AppConfigs { get; set; }
+
+
+        public DbSet<Cart> Carts { get; set; }
+
+       
+        public DbSet<ProductInCategory> ProductInCategories { get; set; }
+
+        public DbSet<Contact> Contacts { get; set; }
+
+       
+
+        public DbSet<Order> Orders { get; set; }
+
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+       
+
+        public DbSet<Promotion> Promotions { get; set; }
+
+
+        public DbSet<Transaction> Transactions { get; set; }
     }
 }
